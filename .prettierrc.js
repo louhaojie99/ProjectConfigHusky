ac69205661b330b5a6dad56cfa3f406a13a5a92e
lhj <851681631@qq.com>
2023-04-09 20:45:51 +0800

    feat: test Update Conf

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,13 +1,12 @@
-
 module.exports = {
   /** 句尾添加分号 */
-  "semi": false,
+  semi: true,
   /**  缩进字节数 */
-  "tabWidth": 2,
+  tabWidth: 2,
   /** 在对象或数组最后一个元素后面是否加逗号 */
-  "trailingComma": "none",
+  trailingComma: 'none',
   /** 使用单引号 */
-  "singleQuote": true,
+  singleQuote: true,
   /** (x) => {} 箭头函数参数只有一个时是否要有小括号。avoid 省略括号 */
-  "arrowParens": "avoid"
-}
+  arrowParens: 'avoid'
+};
